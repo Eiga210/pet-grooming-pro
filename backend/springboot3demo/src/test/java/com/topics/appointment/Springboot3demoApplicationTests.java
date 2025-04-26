e983e20357a4bf1ac216f90ee4be7c646b6cc04e
xin <s415003.ykvs@gmail.com>
2025-04-26 14:36:54 +0800

    Add backend project to main repo

---
+++ backend/springboot3demo/src/test/java/com/topics/appointment/Springboot3demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topics.appointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
